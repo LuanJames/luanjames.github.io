454d7a752d1d8e2cb0f30d8d56c49b6027930c4b
Luan James <capjam@gmail.com>
2016-08-10 14:28:47 -0300

    Add content

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    let data = {
+      "experiences": [
+        {
+          "id": 2,
+          "title": "Engenheiro de Software",
+          "subtitle": "Lab. de Desenvolvimento de Software",
+          "period": "05/2016 - Atual",
+          "description": "Trabalhando em projetos com desenvolvimento Android e PHP.",
+          "acts": ["PHP", "Android"]
+        },
+        {
+          "id": 3,
+          "title": "Líder Técnico",
+          "subtitle": "Lab. de Inovação Tecnológica",
+          "period": "02/2013 - 05/2016",
+          "description": "Trabalhei com desenvolvimento web (Ruby on Rails) e mobile (Android e iOS). " +
+                          "Fiz pesquisas relacionadas à Rede de Petri. Assim como participando de projetos " +
+                          "envolvendo sistemas embarcados.",
+          "acts": ["Ruby on Rails", "PHP", "Android", "DevOps"]
+        }
+      ],
+      "educations": [
+        {
+          "id": 2,
+          "title": "Engenharia de Computação",
+          "subtitle": "IFCE",
+          "period": "2010 - 2015",
+          "description": "Formação em engenharia de computação com ênfase em análise e desenvolvimento de software."
+        }
+      ]
+    };
+    return data;
+  }
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,12 +1,19 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var pickFiles = require('broccoli-static-compiler');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
 
+  app.import('bower_components/materialize/dist/css/materialize.min.css');
+
+  var bootstrapFonts = pickFiles('bower_components/materialize/dist/fonts/roboto', {
+      srcDir: '/',
+      destDir: '/fonts/roboto'
+  });
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
@@ -20,5 +27,5 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  return app.toTree([bootstrapFonts]);
 };
